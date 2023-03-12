e92b020127e7873e083c7b381761e4cf2f6f2201
chochohert <chohee1231@gmail.com>
2023-03-13 03:52:55 +0900

    add .env.production

--- server/config/config.js
+++ server/config/config.js
@@ -1,21 +1,20 @@
 require('dotenv').config()
-const env = process.env
 
 const local = {
-  "username": env.DB_USERNAME,
-  "password": env.DB_PASSWORD,
-  "database": env.DB_NAME,
-  "host": env.DB_HOSTNAME,
-  "port": env.DB_PORT,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "host": process.env.DB_HOSTNAME,
+  "port":process.env.DB_PORT,
   "dialect": "mysql"
 }
 
 const development = {
-  "username": env.DB_USERNAME,
-  "password": env.DB_PASSWORD,
-  "database": env.DB_NAME,
-  "host": env.DB_HOSTNAME,
-  "port": env.DB_PORT,
+  "username": process.env.DB_USERNAME,
+  "password": process.env.DB_PASSWORD,
+  "database": process.env.DB_NAME,
+  "host": process.env.DB_HOSTNAME,
+  "port": process.env.DB_PORT,
   "dialect": "mysql"
 }
 
@@ -29,3 +28,4 @@ const production = {
 }
 
 module.exports = { development, production, local };
+
