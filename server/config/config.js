887060f7226b85ab5badf14ae2cc27a01009105a
chochohert <chohee1231@gmail.com>
2023-03-13 08:35:15 +0900

    change database config

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const local = {
+const config = {
   "username": process.env.DB_USERNAME,
   "password": process.env.DB_PASSWORD,
   "database": process.env.DB_NAME,
@@ -9,23 +9,5 @@ const local = {
   "dialect": "mysql"
 }
 
-const development = {
-  "username": process.env.DB_USERNAME,
-  "password": process.env.DB_PASSWORD,
-  "database": process.env.DB_NAME,
-  "host": process.env.DB_HOSTNAME,
-  "port": process.env.DB_PORT,
-  "dialect": "mysql"
-}
-
-const production = {
-  "username": process.env.DB_USERNAME,
-  "password": process.env.DB_PASSWORD,
-  "database": process.env.DB_NAME,
-  "host": process.env.DB_HOSTNAME,
-  "port": process.env.DB_PORT,
-  "dialect": "mysql"
-}
-
-module.exports = { development, production, local };
+module.exports = config
 
